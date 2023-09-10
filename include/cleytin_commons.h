137a2ffac98d118790b33b206e9fc3ece5b03990
Matheus Alves <matheuseejam@gmail.com>
2023-09-10 11:46:44 -0300

    Corrige tamanho da game rom

--- include/cleytin_commons.h
+++ include/cleytin_commons.h
@@ -12,7 +12,7 @@
 #include "esp_timer.h"
 
 #define CLEYTIN_GAME_ROM_ADDR 0x10000
-#define CLEYTIN_GAME_ROM_SIZE (3 * 1024 * 1024) // 3 Megabytes
+#define CLEYTIN_GAME_ROM_SIZE (15 * 1024 * 1024) // 15 Megabytes
 #define CLEYTIN_BOOT_FLAG_LOAD_GAME_ROM 0xFFFFFFFF
 #define CLEYTIN_BUFFER_SIZE (4 * 1024)
 
